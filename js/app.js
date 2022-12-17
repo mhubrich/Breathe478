fd6e3c309325e246d4bf157511e04804f731dd3c
mhubrich <mhubrich.2011@gmail.com>
2022-12-17 10:51:34 -0800

    change the Animator's 'max' property to 'duration'

--- js/app.js
+++ js/app.js
@@ -5,16 +5,16 @@ const { animate, linear } = popmotion;
  *
  * It controlls two animations: 1) update the text countdown, and 2) animate the circle.
  *
- * @param {String}  label   The label to use for the corresponding phase (e.g. breathe in).
- * @param {Number}  max     Duration of the animation.
- * @param {Object}  title   HTML element to hold the label string.
- * @param {Object}  circle  HTML element to hold the circle.
+ * @param {String}  label       The label to use for the corresponding phase (e.g. breathe in).
+ * @param {Number}  duration    Duration of the animation.
+ * @param {Object}  title       HTML element to hold the label string.
+ * @param {Object}  circle      HTML element to hold the circle.
  */
 class Animator {
 
-    constructor(label, max, title, circle) {
+    constructor(label, duration, title, circle) {
       this._label = label;
-      this._max = max;
+      this._duration = duration;
       this._title = title;
       this._circle = circle;
       this._playback = undefined;
@@ -22,9 +22,9 @@ class Animator {
   
     start(controller) {
       this._playback = animate({
-        from: this._max,
+        from: this._duration,
         to: 0,
-        duration: this._max * 1000,
+        duration: this._duration * 1000,
         type: 'keyframes',
         ease: linear,
         onUpdate: latest => this.update(latest),
@@ -61,18 +61,18 @@ class Animator {
   }
 
   class InAnimator extends Animator {
-    constructor(label, max, title, circle) {
-      super(label, max, title, circle); 
+    constructor(label, duration, title, circle) {
+      super(label, duration, title, circle); 
     }
   
     toPercent(value) {
-      return Math.round(100 * (1 - (value / this._max))) + '%';
+      return Math.round(100 * (1 - (value / this._duration))) + '%';
     }
   }
   
   class HoldAnimator extends Animator {
-    constructor(label, max, title, circle) {
-      super(label, max, title, circle); 
+    constructor(label, duration, title, circle) {
+      super(label, duration, title, circle); 
     }
   
     toPercent(value) {
@@ -81,11 +81,11 @@ class Animator {
   }
   
   class OutAnimator extends Animator {
-    constructor(label, max, title, circle) {
-      super(label, max, title, circle); 
+    constructor(label, duration, title, circle) {
+      super(label, duration, title, circle); 
     }
   
     toPercent(value) {
-      return Math.round(100 * (value / this._max)) + '%';
+      return Math.round(100 * (value / this._duration)) + '%';
     }
   }

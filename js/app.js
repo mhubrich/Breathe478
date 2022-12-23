a0ab866d4e65580b73f3a7181c55c5461720fdd2
mhubrich <mhubrich.2011@gmail.com>
2022-12-23 07:13:41 -0800

    add start and stop buttons with functionality

--- js/app.js
+++ js/app.js
@@ -1,8 +1,27 @@
-// Test the Controller and Animators
-const title = document.getElementById('title');
-const circle = document.getElementById('circle');
-const inAnimator = new InAnimator('Breathe in for ', 4, title, circle);
-const holdAnimator = new HoldAnimator('Hold breath for ', 7, title, circle);
-const outAnimator = new OutAnimator('Breathe out for ', 8, title, circle);
-const controller = new Controller([inAnimator, holdAnimator, outAnimator], 3);
-controller.start();
+// References to UI elements
+const btn_start = document.getElementById('start');
+const btn_stop = document.getElementById('stop');
+
+/**
+* Starts the 4-7-8 breathing technique.
+*
+* The function creates all necessary Animators and Controller, adds a click listener to
+* the stop button, and eventually calls the `start` method of the Controller.
+*
+* @param {Number} breatheIn     Duration of the 'breathe in' animation.
+* @param {Number} hold          duration of the 'hold' animation.
+* @param {Number} breatheOut    Duration of the 'breathe out' animation.
+* @param {Number} repeat        Number of times to cycle through all animations.
+ */
+function start(breatheIn=4, hold=7, breatheOut=8, repeat=3) {
+    const title = document.getElementById('title');
+    const circle = document.getElementById('circle');
+    const inAnimator = new InAnimator('Breathe in for ', breatheIn, title, circle);
+    const holdAnimator = new HoldAnimator('Hold breath for ', hold, title, circle);
+    const outAnimator = new OutAnimator('Breathe out for ', breatheOut, title, circle);
+    const controller = new Controller([inAnimator, holdAnimator, outAnimator], repeat);
+    btn_stop.addEventListener('click', () => controller.stop());
+    controller.start();
+}
+
+btn_start.addEventListener('click', () => start());

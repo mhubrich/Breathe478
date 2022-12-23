2f7496456c697cb6f3aa30feb798f948365fa48e
mhubrich <mhubrich.2011@gmail.com>
2022-12-23 07:42:16 -0800

    start and stop button change visibility of UI elements to toggle between landing page and animation views

--- js/app.js
+++ js/app.js
@@ -1,6 +1,29 @@
 // References to UI elements
 const btn_start = document.getElementById('start');
 const btn_stop = document.getElementById('stop');
+const title = document.getElementById('title');
+const circle = document.getElementById('circle');
+
+
+/**
+* Changes visibility of UI elements to show the landing page view.
+ */
+function landingUI() {
+    btn_start.style.visibility = 'visible';
+    btn_stop.style.visibility = 'hidden';
+    title.style.visibility = 'hidden';
+    circle.style.visibility = 'hidden';
+}
+
+/**
+* Changes visibility of UI elements to show the animation view.
+ */
+function animationUI() {
+    btn_start.style.visibility = 'hidden';
+    btn_stop.style.visibility = 'visible';
+    title.style.visibility = 'visible';
+    circle.style.visibility = 'visible';
+}
 
 /**
 * Starts the 4-7-8 breathing technique.
@@ -14,14 +37,13 @@ const btn_stop = document.getElementById('stop');
 * @param {Number} repeat        Number of times to cycle through all animations.
  */
 function start(breatheIn=4, hold=7, breatheOut=8, repeat=3) {
-    const title = document.getElementById('title');
-    const circle = document.getElementById('circle');
     const inAnimator = new InAnimator('Breathe in for ', breatheIn, title, circle);
     const holdAnimator = new HoldAnimator('Hold breath for ', hold, title, circle);
     const outAnimator = new OutAnimator('Breathe out for ', breatheOut, title, circle);
     const controller = new Controller([inAnimator, holdAnimator, outAnimator], repeat);
-    btn_stop.addEventListener('click', () => controller.stop());
+    btn_stop.addEventListener('click', () => {controller.stop(); landingUI()});
     controller.start();
 }
 
-btn_start.addEventListener('click', () => start());
+btn_start.addEventListener('click', () => {animationUI(); start()});
+landingUI();

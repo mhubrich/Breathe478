6f8e3a5d4b8f1df0c72cf95fa4ebb8aff70d155f
mhubrich <mhubrich.2011@gmail.com>
2022-12-24 09:14:01 +0100

    organize UI elements into landing page view and animation view

--- js/app.js
+++ js/app.js
@@ -1,49 +1,42 @@
-// References to UI elements
-const btn_start = document.getElementById('start');
-const btn_stop = document.getElementById('stop');
-const instruction = document.getElementById('instruction');
-const circle = document.getElementById('circle');
-
-
 /**
-* Changes visibility of UI elements to show the landing page view.
+ * Toggles the UI to display the landing page view.
  */
 function landingUI() {
-    btn_start.style.visibility = 'visible';
-    btn_stop.style.visibility = 'hidden';
-    instruction.style.visibility = 'hidden';
-    circle.style.visibility = 'hidden';
+    document.getElementById('animation').style.display = 'none';
+    document.getElementById('landing').style.display = 'flex';
 }
 
 /**
-* Changes visibility of UI elements to show the animation view.
+ * Toggles the UI to display the animation view.
  */
 function animationUI() {
-    btn_start.style.visibility = 'hidden';
-    btn_stop.style.visibility = 'visible';
-    instruction.style.visibility = 'visible';
-    circle.style.visibility = 'visible';
+    document.getElementById('landing').style.display = 'none';
+    document.getElementById('animation').style.display = 'flex';
 }
 
 /**
-* Starts the 4-7-8 breathing technique.
-*
-* The function creates all necessary Animators and Controller, adds a click listener to
-* the stop button, and eventually calls the `start` method of the Controller.
-*
-* @param {Number} breatheIn     Duration of the 'breathe in' animation.
-* @param {Number} hold          duration of the 'hold' animation.
-* @param {Number} breatheOut    Duration of the 'breathe out' animation.
-* @param {Number} repeat        Number of times to cycle through all animations.
+ * Starts the 4-7-8 breathing technique.
+ *
+ * The function creates all necessary Animators and Controller, adds a click listener to
+ * the stop button, and eventually calls the `start` method of the Controller.
+ *
+ * @param {Number} breatheIn     Duration of the 'breathe in' animation.
+ * @param {Number} hold          duration of the 'hold' animation.
+ * @param {Number} breatheOut    Duration of the 'breathe out' animation.
+ * @param {Number} repeat        Number of times to cycle through all animations.
  */
 function start(breatheIn=4, hold=7, breatheOut=8, repeat=3) {
+    const instruction = document.getElementById('instruction');
+    const circle = document.getElementById('circle');
     const inAnimator = new InAnimator('Breathe in for ', breatheIn, instruction, circle);
     const holdAnimator = new HoldAnimator('Hold breath for ', hold, instruction, circle);
     const outAnimator = new OutAnimator('Breathe out for ', breatheOut, instruction, circle);
     const controller = new Controller([inAnimator, holdAnimator, outAnimator], repeat);
+    const btn_stop = document.getElementById('stop');
     btn_stop.addEventListener('click', () => {controller.stop(); landingUI()});
     controller.start();
 }
 
+const btn_start = document.getElementById('start');
 btn_start.addEventListener('click', () => {animationUI(); start()});
 landingUI();
